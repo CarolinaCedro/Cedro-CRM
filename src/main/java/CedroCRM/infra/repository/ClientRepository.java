546280a1a9d04c71ca8f5e262fd8222550bfe089
CarolinaCedro <yotechgirl@gmail.com>
2022-11-16 15:57:30 -0300

    feat: create repository for de class service and client

---
+++ src/main/java/CedroCRM/infra/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package CedroCRM.infra.repository;
+
+import CedroCRM.infra.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client,Long> {
+}
---
+++ src/main/java/CedroCRM/infra/repository/ServiceRepository.java
@@ -0,0 +1,9 @@
+package CedroCRM.infra.repository;
+
+import CedroCRM.infra.model.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<Service,Long>{
+}
