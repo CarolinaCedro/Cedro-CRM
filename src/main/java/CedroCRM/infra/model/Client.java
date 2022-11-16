f522f9574fd895baf3cc44fe0f48ba65a6220f86
CarolinaCedro <yotechgirl@gmail.com>
2022-11-16 15:48:04 -0300

    feat: create entity client

--- src/main/java/CedroCRM/infra/model/Client.java
+++ src/main/java/CedroCRM/infra/model/Client.java
@@ -1,5 +1,21 @@
 package CedroCRM.infra.model;
 
-@Ent
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table
+@Data
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false,length = 150)
+    private String name;
+    @Column(nullable = false,length = 11)
+    private String cpf;
+    @Column
+    private LocalDate registrationTime;
 }
